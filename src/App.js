1e36ae753322ee7f10ef2b4c092aca989fbf2377
Shivayshiva <prashantmaddheshiya@gmail.com>
2022-12-27 23:48:43 +0530

    Third commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { getDatabase, ref, push, set, onChildAdded } from "firebase/database";
 
 function App() {
+  const [name, setName] = useState("");
+  const [chats, setChats] = useState([]);
+  const [msg, setMsg] = useState("");
+  const db = getDatabase();
+  const chatListRef = ref(db, "chats");
+
+  useEffect(() => {
+    onChildAdded(chatListRef, (data) => {
+      setChats((chats) => [...chats, data.val()]);
+    });
+  }, []);
+
+  const sendChat = () => {
+    const chatRef = push(chatListRef);
+    set(chatRef, { name, message: msg });
+    // const c = [...chats];
+    // c.push({name, message:msg});
+    // setChats(c);
+    setMsg("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {name ? null : (
+        <div className="input-div">
+          <input
+            className="input-field"
+            type="text"
+            placeholder="Enter Name"
+            onBlur={(e) => setName(e.target.value)}
+          />
+        </div>
+      )}
+      {name ? (
+        <div>
+          <h1>User:{name}</h1>
+          <div className="chat-container">
+            {chats.map((c) => (
+              <div className={`container ${c.name === name ? "me" : ""}`}>
+                <p className="chatbox">
+                  <strong>{c.name}:</strong>
+                  <span>{c.message}</span>
+                </p>
+              </div>
+            ))}
+            {/* <div className='container'>
+            <p className='chatbox'>
+              <strong>name:</strong>
+              <span>chat message</span>
+            </p>
+        </div> */}
+        </div>
+        <div className="btm">
+            <input
+              type="text"
+              value={msg}
+              onChange={(e) => setMsg(e.target.value)}
+              placeholder="Enter Your Messae"
+            />
+            <button onClick={() => sendChat()}>Send.</button>
+        </div>
+      </div>
+      ) : null}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBAvYpabUk07_d6PVchJbwKBaoPtRRehQI",
+  authDomain: "chat-app-react-f8320.firebaseapp.com",
+  databaseURL: "https://chat-app-react-f8320-default-rtdb.firebaseio.com",
+  projectId: "chat-app-react-f8320",
+  storageBucket: "chat-app-react-f8320.appspot.com",
+  messagingSenderId: "77337740828",
+  appId: "1:77337740828:web:58b6a37d5a6507248701d9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
